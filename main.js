ea7ba2cc2fa7b2d983c4f278ff8324c0000bc65f
Nathee Sungthong-ngam <natee.live@gmail.com>
2020-11-26 10:09:10 +0700

    remove server folder in monitor-ui

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+// const isDev = require('electron-is-dev'); 
 const path = require('path')
-const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,16 +10,19 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({ 
-    width: 350,
-    height: 400,
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    }
   })
 
-  mainWindow.loadURL('http://localhost:3333');
-  mainWindow.setMenu(null);
-  mainWindow.setResizable(false);
+  const startURL = `file://${path.join(__dirname, './monitor-ui/build/index.html')}`;
+  mainWindow.loadURL(startURL)
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
